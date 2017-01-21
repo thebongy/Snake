58eb6fd87f63621192cd2fec7529e63a3c9c09e1
thebongy <rishit.bansal0@gmail.com>
2017-01-21 23:05:41 +0530

    Major Update: >snake now renders on screen >input loop modified to use dict value reference >Screen represented as x-y plane with origin (0,0) at top left. >Snake moving functionality with Insertion and Pop >Screen is now a matrix, not a array of strings

--- Snake.py
+++ Snake.py
@@ -20,21 +20,21 @@ def UnixController(game,snake):
 
 
 def WindowsController(game,snake):
-    from msvcrt import getch
-    while game['running']:
-        ch = ord(getch())
-        if ch == 224:
-            ch = ord(getch())
-            if ch == 72:
-                snake['direction'] = 0
-            elif ch == 80:
-                snake['direction'] = 1
-            elif ch == 77:
-                snake['direction'] = 2
-            elif ch == 75:
-                snake['direction'] = 3
-        elif ch == 27 or ch == 112:
-            game['state'] = 2
+	from msvcrt import getch
+	current = snake['direction']
+	dir = {72:0,80:2,77:1,75:3}
+	while game['running']:
+		ch = ord(getch())
+		if ch == 224:
+			ch = ord(getch())
+			code = dir.get(ch)
+			if code != current^2:
+				snake['direction'] = code
+				current = code
+		elif ch == 27 or ch == 112:
+			game['running'] = False
+			clear_screen()
+            #game['state'] = 2 #Pause game functionality to be implemented
 
 
 def UnixResize(width,height):
@@ -43,46 +43,72 @@ def UnixResize(width,height):
 def WindowsResize(width,height):
 	system('mode %s,%s' % (width,height))
 
-if __UserOS__ == 'nts':
-    keyInput = WindowsController
-    clear_screen = lambda:system('cls')
+if __UserOS__ == 'nt':
+	keyInput = WindowsController
+	clear_screen = lambda:system('cls')
 	resize = WindowsResize
 else:
-    keyInput = UnixController
-    clear_screen = lambda:system('clear')
+	keyInput = UnixController
+	clear_screen = lambda:system('clear')
 	resize = UnixResize
 
 
 def Game(game,snake):
+	position = snake['position']
+	printable = game['screen']
+	
+	x,y = position[0]
+	printable[y][x] = 'S'
+	
+	for pos in position[1:]:
+		x,y = pos
+		printable[y][x] = 'o'
+	
 	while game['running']:
 		direction = snake['direction']
-		x,y = snake['position'][0]
+		x1,y1 = position[0]
 		if direction % 2:
-			new_pos = (x-(direction-2),y)
+			x2,y2 = (x1-(direction-2),y1)
 		else:
-			new_pos = (x,y+(direction-1))
-		#snake['position'][0] = new_pos
-		#Shift all coordinates in [1:] to right by one. Delete last coordinate
-		#snake['position'][1] = direction^2
-	
-	
+			x2,y2 = (x1,y1+(direction-1))
+		
+		x3,y3 = position[-1]
+		
+		printable[y1][x1] = 'o'
+		printable[y2][x2] = 'S'
+		printable[y3][x3] = ' '
+		
+		position.insert(0,(x2,y2))
+		position.pop()
+		
+		clear_screen()
+		
+		for row in printable:
+			print ''.join(row)
+		
+		sleep(0.04)
 
-def main():
+		
+def main(width,height):
 	game = {\
 				'running':False,\
-				'screen':[' '*(width-1) for i in range(height)],\
+				'screen':[[' ' for i in range(width-1)] for i in range(height)],\
 				'score':0,\
 				'food':tuple()
 			}
 	
-	
+	x,y = (width/2,height/2)
 	snake = {\
 				'direction':1,\
-				'position':[(width/2,height/2)]\
+				'position':[(x-i,y) for i in range(1,20)]\
 			}
 	
 	
+	resize(width,height)
 	game['running'] = True
 	controller = Thread(target=keyInput,args=(game,snake))
+	controller.setDaemon(True)
 	controller.start()
 	Game(game,snake)
+
+main(50,30)
